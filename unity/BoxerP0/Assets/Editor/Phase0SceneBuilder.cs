5d96da94b8821b05bb5da0f3ddd4479ecfaadf8b
Tri Nguyen <minhtri22@gmail.com>
2026-09-05 13:56:47 +0700

    build: record non-blocking motion startup and strict source provenance

--- unity/BoxerP0/Assets/Editor/Phase0SceneBuilder.cs
+++ unity/BoxerP0/Assets/Editor/Phase0SceneBuilder.cs
@@ -118,6 +118,8 @@ namespace BoxerP0.Editor
                 $"p1_a1=step_to_straight_reach_1.06_1.00_0.94\n" +
                 $"p1_a2=tap_straight_hold_up_uppercut_hold_horizontal_hook_hold_down_overhand\n" +
                 $"p1_b1=locked_opponent_target_finite_reach_no_homing\n" +
+                $"startup_gate=motion_permission_unblocks_gameplay_orientation_optional_late_neutral\n" +
+                $"source_provenance=build_marker_must_match_clean_committed_source_head\n" +
                 $"build_commit={buildMarker}\n");
             Debug.Log($"P0_WEB_BUILD={buildDir} marker={buildMarker} data={dataSha256} wasm={wasmSha256}");
         }
